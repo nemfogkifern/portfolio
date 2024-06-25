bd7a8db66625ef13c99f5697eaafdbd8ab8daf5f
nemfogkifern <gyurinabercel@gmail.com>
2024-06-25 13:08:00 +0200

    as

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Burger menu
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('nav').classList.toggle('active');
     document.querySelector('.overlay').classList.toggle('active');
@@ -13,4 +15,21 @@ document.querySelectorAll('nav ul li a').forEach(function(item) {
         document.querySelector('nav').classList.remove('active');
         document.querySelector('.overlay').classList.remove('active');
     });
+});
+
+// Social buttons
+document.querySelector('.Btn.github').addEventListener('click', function() {
+    window.open('https://github.com', '_blank');
+});
+
+document.querySelector('.Btn.whatsapp').addEventListener('click', function() {
+    window.open('https://whatsapp.com', '_blank');
+});
+
+document.querySelector('.Btn.linkdin').addEventListener('click', function() {
+    window.open('https://linkdin.com', '_blank');
+});
+
+document.querySelector('.Btn.discord').addEventListener('click', function() {
+    window.open('https://discord.com', '_blank');
 });
