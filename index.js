4a5f6bf2dcb79fb420c4f6e3ac375060ff7b966c
nemfogkifern <gyurinabercel@gmail.com>
2024-06-26 01:20:53 +0200

    sok minden megcsinalva

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 
 // Burger menu
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('active');
-    document.querySelector('.overlay').classList.toggle('active');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    const overlay = document.querySelector('.overlay');
+    const menuLinks = document.querySelectorAll('nav ul li a');
 
-document.querySelector('.overlay').addEventListener('click', function() {
-    document.querySelector('nav').classList.remove('active');
-    document.querySelector('.overlay').classList.remove('active');
-});
+    // Toggle menu and overlay on menu toggle click
+    menuToggle.addEventListener('click', function() {
+        nav.classList.toggle('active');
+        overlay.classList.toggle('active');
+    });
 
-document.querySelectorAll('nav ul li a').forEach(function(item) {
-    item.addEventListener('click', function() {
-        document.querySelector('nav').classList.remove('active');
-        document.querySelector('.overlay').classList.remove('active');
+    // Close menu and overlay on overlay click
+    overlay.addEventListener('click', function() {
+        nav.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
+    // Close menu and overlay on menu link click
+    menuLinks.forEach(function(item) {
+        item.addEventListener('click', function() {
+            nav.classList.remove('active');
+            overlay.classList.remove('active');
+        });
     });
 });
+
+
 
 // Social buttons
 document.querySelector('.Btn.github').addEventListener('click', function() {
@@ -32,4 +44,22 @@ document.querySelector('.Btn.linkdin').addEventListener('click', function() {
 
 document.querySelector('.Btn.discord').addEventListener('click', function() {
     window.open('https://discord.com', '_blank');
+});
+
+// scroll textanimation
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hiddenText = document.querySelector('.hidden-text');
+
+    function checkPosition() {
+        const rect = hiddenText.getBoundingClientRect();
+        const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+        if (rect.top <= windowHeight) {
+            hiddenText.classList.add('show');
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    checkPosition(); // initial check in case the element is already in view
 });
